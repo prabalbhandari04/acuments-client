7c3b572ce93c412c8d46268cb0c2bda478406951
Prabal Bhandari <54536906+prabalbhandari04@users.noreply.github.com>
2023-04-09 13:38:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
