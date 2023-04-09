381ad8c6570b82d295ce105074882f6eb03777f4
Prabal Bhandari <54536906+prabalbhandari04@users.noreply.github.com>
2023-04-09 08:39:18 +0530

    Update MovieList.js

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import MovieTile from './MovieTile';
 
-function MovieList() {
-  const movies = useSelector((state) => state.movies);
+const MovieList = () => {
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [movie, setMovie] = useState([])
 
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get('http://localhost:5050/movies', {
+        params: {
+          page,
+        },
+      });
+      const { results, total_pages } = response.data;
+      setMovie(prevMovies => [...prevMovies, ...results]);
+      setHasMore(page < total_pages);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+
+  const handleLoadMore = () => {
+    if (!isLoading) {
+      setPage(page + 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [page]);
+  console.log(movie)
   return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <MovieTile key={movie.id} movie={movie} />
-      ))}
-    </div>
+    // <div>
+    //     <h1>Tetst</h1>
+    // </div>
+    <InfiniteScroll
+      dataLength={movie.length}
+      next={handleLoadMore}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+    >
+      <div className="movie-list">
+        {movie.map((movie) => (
+          <MovieTile key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </InfiniteScroll>
   );
-}
+};
 
 export default MovieList;
