9e85bbea9bfbf8f4be496f9d8af97ef0202b5a5b
Prabal Bhandari <54536906+prabalbhandari04@users.noreply.github.com>
2023-04-09 08:37:04 +0530

    movie tile changes

--- src/MovieTile.js
+++ src/MovieTile.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
-function MovieTile({ movie }) {
+const MovieTile = ({ movie }) => {
+
+  const handleAddToWatchlist = () => {
+    alert('Add to Watchlist')
+  };
+
   return (
     <div className="movie-tile">
-      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-      <div className="movie-details">
-        <h2>{movie.title}</h2>
-        <p>{movie.release_date}</p>
-        <p>Rating: {movie.vote_average}</p>
-        <p>{movie.overview}</p>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        alt={movie.title}
+        className="movie-tile__poster"
+      />
+      <div className="movie-tile__info">
+        <h3 className="movie-tile__title">{movie.title}</h3>
+        <p className="movie-tile__release-date">
+          {new Date(movie.release_date).toLocaleDateString()}
+        </p>
+        <p className="movie-tile__rating">{movie.vote_average}/10</p>
+        <button onClick={handleAddToWatchlist}>Add to watchlist</button>
       </div>
     </div>
   );
-}
+};
 
 export default MovieTile;
